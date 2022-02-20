80c908cbf10435deab4f5c7b77734e0a38908804
GordonZhou1998 <87012118+GordonZhou1998@users.noreply.github.com>
2022-02-20 15:25:57 +0800

    Lzl_graduation_design

---
+++ src/main/java/com/neusoft/zywlzl/ZywlzlApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.zywlzl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZywlzlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZywlzlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neusoft/zywlzl/ZywlzlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neusoft.zywlzl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZywlzlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
